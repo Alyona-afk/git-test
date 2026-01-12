3c161df61b98801597896f5f3a200a50c15aa73c
Alyona-afk <alenapivovarovav@gmail.com>
2026-01-12 18:04:52 +0000

    Привет, Алёна!

--- hello.c
+++ hello.c
@@ -1,11 +1,6 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main(void) {
-    int width = 10;
-    int height = 10;
-
-    printf("Width: %d\n", width);
-    printf("Height: %d\n", height);
-
-    return 0;
+int main(void){
+printf("Hello, Alyona!\n");
+return 0;
 }
