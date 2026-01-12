1a54a4168f550290ded9d1e1817faa490c693079
Alyona-afk <alenapivovarovav@gmail.com>
2026-01-12 19:04:03 +0000

    Новая папка

---
+++ Nih/Nihao.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("====================\n");
+	printf("Welcome to my C program\n");
+	printf("This is my first step\n");
+	printf("====================\n");
+	return 0;
+}
