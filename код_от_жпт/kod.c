14f77fcc4df86e83c59a7ad5b0f5323a05aad208
Alyona-afk <alenapivovarovav@gmail.com>
2026-01-13 13:01:20 +0000

    Тоже новая папка

---
+++ код_от_жпт/kod.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int age = 25;
+    float height = 1.68;
+    char grade = 'A';
+
+    printf("Age: %d\n", age);
+    printf("Height: %.2f\n", height);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
